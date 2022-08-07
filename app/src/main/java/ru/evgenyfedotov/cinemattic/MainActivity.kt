1f54658d95035d775e430a44a1c04178f60253a1
Evgeny Fedotov <e.a.fedotov@gmail.com>
2022-08-07 20:55:41 +0300

    Working Crashlytics

--- app/src/main/java/ru/evgenyfedotov/cinemattic/MainActivity.kt
+++ app/src/main/java/ru/evgenyfedotov/cinemattic/MainActivity.kt
@@ -2,6 +2,8 @@ package ru.evgenyfedotov.cinemattic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.recyclerview.widget.RecyclerView
