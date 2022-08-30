f5ca806db5d98a41a7979ad16a6697eefde306c6
Evgeny Fedotov <e.a.fedotov@gmail.com>
2022-08-30 12:09:40 +0300

    Firebase messaging works in Foreground and Background

--- app/src/main/java/ru/evgenyfedotov/cinemattic/firebase/CloudNotificationService.kt
+++ app/src/main/java/ru/evgenyfedotov/cinemattic/firebase/CloudNotificationService.kt
@@ -5,8 +5,11 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
 import android.util.Log
 import androidx.core.app.NotificationCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.Target
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import okhttp3.internal.notify
@@ -38,6 +41,13 @@ class CloudNotificationService : FirebaseMessagingService(){
             newIntent,
             PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
         )
+
+        val notificationImage: Bitmap = Glide.with(this)
+            .asBitmap()
+            .load(message.notification?.imageUrl)
+            .submit()
+            .get()
+
         val notification = NotificationCompat.Builder(this,
             AlarmNotificationReceiver.CHANNEL_ID
         )
@@ -48,6 +58,8 @@ class CloudNotificationService : FirebaseMessagingService(){
             .setContentIntent(pendingIntent)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
             .setAutoCancel(true)
+            .setStyle(NotificationCompat.BigPictureStyle()
+                .bigPicture(notificationImage))
             .build()
 
         Log.d("notification", "onReceive: Here is notification")
